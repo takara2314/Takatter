3223ad20f39529343dfd56c950f02484d8fbd638
takara2314 <takara2314_TK@outlook.jp>
2020-04-16 10:18:53 +0900

    最初のコミット

---
+++ lib/home.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'takert.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: basicAppBar(),
+      body: Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          children: <Widget>[
+            takertLine(),
+          ],
+        ),
+      ),
+      floatingActionButton: lowerRightButton(),
+    );
+  }
+
+  // 標準的なアプリバー
+  Widget basicAppBar() {
+    return AppBar(
+      leading: Icon(Icons.account_circle,
+        size: 40.0,
+      ),
+      title: Text('Takatter',
+        style: TextStyle(
+          fontSize: 30.0,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      backgroundColor: Color.fromARGB(255, 0, 200, 150),
+      centerTitle: true,
+      actions: <Widget>[
+        IconButton(
+          icon: Icon(Icons.search,
+            color: Colors.white,
+            size: 30.0,
+          ),
+          onPressed: (){},
+        ),
+      ],
+    );
+  }
+
+  // タカートライン(タカートを表示するタイムライン)
+  Widget takertLine() {
+    return Expanded(
+      child: ListView.builder(
+        scrollDirection: Axis.vertical,
+        shrinkWrap: true,
+        itemCount: takerts.length,
+        itemBuilder: (BuildContext context, int index) {
+          final takert = takerts[index];
+
+          return Card(
+            child: ListTile(
+              leading: Icon(Icons.account_circle,
+                color: Colors.green,
+                size: 40.0,
+              ),
+              title: Text(takert, style: TextStyle(
+                fontSize: 20.0,
+              )),
+            )
+          );
+        },
+      )
+    );
+  }
+
+  // タカートを管理するリスト
+  List<String> takerts = ["タカッター始めてみた！って僕一人！？"];
+
+  // 右下のボタン
+  Widget lowerRightButton() {
+    return FloatingActionButton(
+      onPressed: () {
+        lowerRightButtonEvent(context);
+      },
+      tooltip: 'タカートするボタンです。',
+      child: Icon(Icons.airplanemode_active,
+        size: 30.0,
+      ),
+    );
+  }
+
+  // 右下のボタンを押したときの処理
+  Future lowerRightButtonEvent(context) async {
+    String takertText = await Navigator.push(context, MaterialPageRoute(builder: (context) => TakertPage()));
+    if (takertText != null && takertText != "") {
+      print('反映するタカート内容: $takertText');
+      takerts.add(takertText);
+    } else {
+      print('タカートされませんでした…');
+    }
+  }
+}
---
+++ lib/takert.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+
+class TakertPage extends StatefulWidget {
+  @override
+  _TakertPageState createState() => _TakertPageState();
+}
+
+class _TakertPageState extends State<TakertPage> {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: basicAppBar(),
+      body: Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          // mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            takertTextBox(),
+            takertButton(),
+          ],
+        ),
+      ),
+    );
+  }
+    // 標準的なアプリバー
+  Widget basicAppBar() {
+    return AppBar(
+      title: Text('タカートする',
+        style: TextStyle(
+          fontSize: 25.0,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      backgroundColor: Color.fromARGB(255, 0, 200, 150),
+      centerTitle: true,
+    );
+  }
+
+  // タカートするテキストを入れる場所
+  Widget takertTextBox() {
+    return TextField(
+      controller: textController,
+      style: takertTextStyle(),
+      decoration: InputDecoration(
+        // ガイドテキスト
+        hintText: 'いまタカってる？',
+        hintStyle: takertTextStyle(),
+        // 非選択時の外枠の装飾
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Color.fromARGB(255, 0, 200, 0), width: 2.0),
+          borderRadius: BorderRadius.circular(100.0),
+        ),
+        // 選択時の外枠の装飾
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.green, width: 2.0),
+          borderRadius: BorderRadius.circular(20.0),
+        ),
+      ),
+    );
+  }
+
+  // タカートテキストのスタイル
+  TextStyle takertTextStyle() {
+    return TextStyle(
+      fontSize: 20.0,
+      fontWeight: FontWeight.bold,
+    );
+  }
+
+  // タカートボタン
+  Widget takertButton() {
+    return Align(
+      // 右寄せにする
+      alignment: Alignment.centerRight,
+      child: RaisedButton(
+        child: Text('タカート', style: takertTextStyle()),
+        color: Colors.white,
+        shape: takertButtonBorder(),
+        onPressed: takertEvent,
+      )
+    );
+  }
+
+  // タカートボタンの外枠
+  Border takertButtonBorder() {
+    return Border(
+      top: BorderSide(
+        color: Colors.red,
+        width: 4.0,
+      ),
+      left: BorderSide(
+        color: Colors.blue,
+        width: 4.0,
+      ),
+      right: BorderSide(
+        color: Colors.yellow,
+        width: 4.0,
+      ),
+      bottom: BorderSide(
+        color: Colors.green,
+        width: 4.0,
+      ),
+    );
+  }
+
+  // タカートボタンが押された時の処理
+  void takertEvent() {
+    String takertText = textController.text;
+    print('タカートボタンが押されました！');
+    print(takertText);
+    // テキストフィールドの文章を削除
+    textController.clear();
+    // 前のページに戻る
+    Navigator.pop(context, takertText);
+  }
+
+  final textController = TextEditingController();
+}
